6c0f16b276ef59daec5e6b8046c820f00adc0d2e
dohyun97 <dohyun647@gmail.com>
2023-04-13 13:13:49 -0400

    Bean validation

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,27 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+//Object error:이 방법을 추천 안해.오브젝트 오류부분만 직접 코드로 작성하는 것을 권장
+//@ScriptAssert(lang = "javascript",script = "_this.price * _this.quantity >= 10000",message = "총합이 10000원 넘어야 합니다")
 @Data
 public class Item {
 
     private Long id;
+    @NotBlank(message = "공백x")  //" " //message를 사용하면 내가 직접 오류메세지 설정 가(default message) ->messageSource(errors.properties에서 메시지 없으면 적용)
     private String itemName;
+    @NotNull   //0
+    @Range(min=1000,max=1000000)
     private Integer price;
+    @NotNull //0
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
