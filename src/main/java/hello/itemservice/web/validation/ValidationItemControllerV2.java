865ed430e05d11a526839ae293f4f45b2d6f2883
dohyun97 <dohyun647@gmail.com>
2023-04-13 02:29:39 -0400

    Sperate with Validator and controller

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -24,6 +24,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -155,7 +156,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
        //타입 오류와 아래 오류들이 같이 있을 때 타입 오류메세지만 한줄로 보이게 할려고
         if(bindingResult.hasErrors()){
@@ -184,6 +185,23 @@ public class ValidationItemControllerV2 {
 
             }
         }
+        //검증에 실패하면 다시 입력 폼으로. 데이터 itermRepository에 저장 안돼
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}",bindingResult);
+            return "validation/v2/addForm";
+        }
+
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        itemValidator.validate(item,bindingResult);
         //검증에 실패하면 다시 입력 폼으로. 데이터 itermRepository에 저장 안돼
         if(bindingResult.hasErrors()){
             log.info("errors = {}",bindingResult);
