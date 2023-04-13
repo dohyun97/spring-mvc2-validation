60fa602fc9b627c8257522cb04e498ae89298ea4
dohyun97 <dohyun647@gmail.com>
2023-04-13 16:50:22 -0400

    API Validation

--- src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/validation/api/items")
 public class ValidationItemApiController {
-    @PostMapping("/add")
+    @PostMapping("/add")  //이거는 타입메치 에러 메세지가 우리가 만든걸로 안보여
     public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult){
         log.info("API controller");
         if(bindingResult.hasErrors()){
             log.info("검증 오류 발생 {}",bindingResult);
-            return bindingResult.getAllErrors();
+            return bindingResult.getAllErrors();  //모든 오류정보들이 보여. 필요한 데이터만 뽑아야돼
         }
         log.info("성공 로직 실행");
         return form;
